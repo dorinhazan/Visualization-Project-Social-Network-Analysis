b071d709dc2292e39ea54781bcdd56f2b3d35491
nettaya <166116817+nettaya@users.noreply.github.com>
2024-07-15 16:18:13 +0300

    Update visualization.py

--- visualization.py
+++ visualization.py
@@ -286,7 +286,7 @@ else:
     with col2:
         st.pyplot(fig2)
 # Section 5: Pie Charts for Family Status of Users
-st.header('Pie Charts Showing Family Status of Users in {app_1}, {app_2}')
+st.header(f'Pie Charts Showing Family Status of Users in {app_1}, {app_2}')
 
 if 'FamilyStatus' in df.columns and app1_cleaned in df.columns and app2_cleaned in df.columns:
     # Filter dataframe for the selected apps
